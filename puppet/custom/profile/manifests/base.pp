f58ebde511ae51729fcc31a52b9f06b669f85faa
Oliver Davies <oliver@oliverdavies.co.uk>
2015-04-17 10:51:17 +0100

    Install the remi repo

--- puppet/custom/profile/manifests/base.pp
+++ puppet/custom/profile/manifests/base.pp
@@ -1,4 +1,10 @@
 class profile::base {
+  # Install the remi repository.
+  class { 'yum':
+    extrarepo => ['remi'],
+  }
+
+  # Install packages.
   $base_packages = hiera('base::packages')
   package { $base_packages:
   }
