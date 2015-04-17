7ae1bf687220e470bf2d96480fbf03714f0e6566
Oliver Davies <opdavies@gmail.com>
2015-04-17 10:56:55 +0100

    Re-ordered things

--- puppet/custom/profile/manifests/php.pp
+++ puppet/custom/profile/manifests/php.pp
@@ -1,11 +1,12 @@
 class profile::php {
-  include ::apache::mod::php
-
   # Install PHP.
   $php_modules = hiera('php::modules')
   $apache = hiera('apache')
-    package { $php_modules:
-      require => [ Yumrepo['remi'], Package[$apache] ],
-      notify  => Service[$apache]
+  package { $php_modules:
+    require => [ Yumrepo['remi'], Package[$apache] ],
+    notify  => Service[$apache]
   }
+
+  # Enable mod_php.
+  include ::apache::mod::php
 }
