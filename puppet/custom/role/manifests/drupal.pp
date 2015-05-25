b2b17c1cb76cc8c862906935c23f02b93a89e3aa
Oliver Davies <oliver@oliverdavies.co.uk>
2015-05-25 09:15:26 +0100

    Update drupal.pp

--- puppet/custom/role/manifests/drupal.pp
+++ puppet/custom/role/manifests/drupal.pp
@@ -3,11 +3,11 @@ class role::drupal inherits role::web {
   include profile::mysql
   include drush
 
-  drupal_site { 'drupal7.local':
+  drupal_site { 'drupal7':
     enable => true,
   }
 
-  drupal_site { 'drupal8.local':
+  drupal_site { 'drupal8':
     enable => true,
   }
 }
